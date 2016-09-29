1cfe2e3838d73e276d05bd5e64fe3e7da086c52c
richPurba <richardtpurba@richards-mac-mini.local>
2016-09-29 16:39:06 +0200

    Final Constraints for Buttons. Starting to add sounds

--- TrueFalseStarter/ViewController.swift
+++ TrueFalseStarter/ViewController.swift
@@ -13,13 +13,13 @@ import AudioToolbox
 class ViewController: UIViewController {
 
             
-    let questionsPerRound: Int = 4
+    let questionsPerRound: Int = 10
     var questionsAsked: Int = 0
     var correctQuestions: Int = 0
     var indexOfSelectedQuestion: Int = 0
     var gameSound: SystemSoundID = 0
-
-    var triviaCollection = DataTrivia().returningTrivia()
+    var arrayForPostedQuestions: [String] = []
+    var triviaCollection: [[String:String]] = []
 // renaming the Outlet to accomodate 4 options
     
 
@@ -40,7 +40,7 @@ class ViewController: UIViewController {
         playGameStartSound()
         displayQuestion()
         displayOptions()
-
+        buttonsDesign()
     }
 
     override func didReceiveMemoryWarning() {
@@ -52,24 +52,57 @@ class ViewController: UIViewController {
    
     
     func displayQuestion() {//renaming the question for true false
+         triviaCollection = DataTrivia().returningTrivia()
         indexOfSelectedQuestion = GKRandomSource.sharedRandom().nextIntWithUpperBound(triviaCollection.count)
-        let trivia = triviaCollection[indexOfSelectedQuestion]
+        var trivia = triviaCollection[indexOfSelectedQuestion]
+        
+        //Not to post the same question in the same game
+        // i stored the asked questions rather than the index because my Model accomodates two types of Dictionary: TrueFalse and Multiple Choices
+        
+        if questionsAsked != 0 {
+            //restarting the collection to retrive new type of questions
+            
+            
+            // mechanism to recheck the question hasn't been asked before
+            var counter = 0
+            
+            while counter == 0 {
+                for storedQuestion in arrayForPostedQuestions{
+                    if trivia["Question"] == storedQuestion{
+                        counter = 0
+                        
+                        indexOfSelectedQuestion = GKRandomSource.sharedRandom().nextIntWithUpperBound(triviaCollection.count)
+                        trivia = triviaCollection[indexOfSelectedQuestion]
+                        break// break the for loop and start all over again
+                    }
+                }
+                counter = 1//trivia is okay, no recurring question
+            }
+                    }
         outPutQuestion.text = trivia["Question"]
+        arrayForPostedQuestions.append(trivia["Question"]!)
         playAgain.hidden = true
+        showAnswer.hidden = true
     }
     
     func displayOptions(){
+
         let trivia = triviaCollection[indexOfSelectedQuestion]
         
         if trivia.count == 2{//meaning that this is the anatomy of True False DataModel
-            option1.setTitle("True", forState: .Normal)
-            option2.setTitle("False", forState: .Normal)
+        
             option3.hidden = true
             option4.hidden = true
+            
+            option1.setTitle("True", forState: .Normal)
+            option2.setTitle("False", forState: .Normal)
+            
         } else {
+        
             option3.hidden = false
             option4.hidden = false
-            
+
+    
             option1.setTitle(returningTriviaValues(trivia, keyword: "Option1"), forState: .Normal)
             option2.setTitle(returningTriviaValues(trivia, keyword: "Option2"), forState: .Normal)
             option3.setTitle(returningTriviaValues(trivia, keyword: "Option3"), forState: .Normal)
@@ -83,6 +116,9 @@ class ViewController: UIViewController {
         // Hide the answer buttons
         option1.hidden = true
         option2.hidden = true
+        option3.hidden = true
+        option4.hidden = true
+
         
         // Display play again button
         playAgain.hidden = false
@@ -96,34 +132,41 @@ class ViewController: UIViewController {
     @IBAction func checkAnswer(sender: UIButton) {
         // Increment the questions asked counter
         questionsAsked += 1
-        
+        showAnswer.hidden = false
         let trivia = triviaCollection[indexOfSelectedQuestion]
         
         let correctAnswer = returningTriviaValues(trivia, keyword: "Answer")
         
-        if (sender === option1 &&  correctAnswer == "True") || (sender === option2 && correctAnswer == "False") {
+        let currentTitleOption1 = option1.currentTitle
+        let currentTitleOption2 = option2.currentTitle
+        let currentTitleOption3 = option3.currentTitle
+        let currentTitleOption4 = option4.currentTitle
+        
+        if (sender === option1 &&  correctAnswer == currentTitleOption1) || (sender === option2 && correctAnswer == currentTitleOption2) || (sender === option3 && correctAnswer == currentTitleOption3) || (sender === option4 && correctAnswer == currentTitleOption4){
             correctQuestions += 1
-            outPutQuestion.text = "Correct!"
+            showAnswer.text = "Correct!"
         } else {
-            outPutQuestion.text = "Sorry, wrong answer!"
+            showAnswer.text = "Sorry, wrong answer!"
         }
         
         loadNextRoundWithDelay(seconds: 2)
-        
-        //restarting the collection to retrive new type of questions
-         triviaCollection = DataTrivia().returningTrivia()
     }
+    
+    
     
     func nextRound() {
         if questionsAsked == questionsPerRound {
             // Game is over
             displayScore()
+            //removing the values for checking the already posted questions
+            arrayForPostedQuestions.removeAll()
         } else {
             // Continue game
             displayQuestion()
+            displayOptions()
         }
     }
-    // need to rework for 4 options
+    
         // Show the answer buttons
     
     @IBAction func playAgainOption() {
@@ -135,8 +178,7 @@ class ViewController: UIViewController {
         nextRound()
         
 }
-    
-    
+
     // MARK: Helper Methods
     
     func loadNextRoundWithDelay(seconds seconds: Int) {
@@ -172,4 +214,14 @@ class ViewController: UIViewController {
     return withCollection[selectingWhichTrivia]
     }
     
-   }
+    func buttonsDesign(){
+        let cornerRadius: CGFloat = 7.0
+        option1.layer.cornerRadius = cornerRadius
+        option2.layer.cornerRadius = cornerRadius
+        option3.layer.cornerRadius = cornerRadius
+        option4.layer.cornerRadius = cornerRadius
+        playAgain.layer.cornerRadius = cornerRadius
+    }
+
+    
+}
